f1bcd26b1751d4e36ea42cbc891fa3436b34010f
Bernardo <bersuarezsequeira@gmail.com>
2021-09-11 10:15:44 -0600

    readmi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import React, {Fragment} from 'react';
+import List from "./containers/List";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+      <>
+            <nav className="p-4 navbar navbar-dark bg-dark border-bottom border-white">
+              <a href="/" className="navbar-brand">
+                MovieApp
+              </a>
+            </nav>
+              <main className="bg-dark">
+                  <div className="container">
+                      <List/>
+                  </div>
+            </main>
+      </>
+    
+  )
 }
+
 
 export default App;
---
+++ src/containers/List.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+import Card from '../components/card/Card';
+
+//console.log(process.env.API);
+
+const API = 'http://www.omdbapi.com/?i=tt3896198&apikey=e6db1a5'
+
+//const API = process.env.API;
+
+class List extends React.Component{
+
+  constructor(){
+    super();
+    this.state = {
+      data:[],
+      searchTerm: '',
+      error: '',
+      loading: true
+    };
+  }
+  
+  async componentDidMount(){
+  // const res = await fetch('../../assets/data.json')
+  const res = await fetch(`${API}&s=batman`)
+   const resJSON = await res.json();  
+   this.setState({data: resJSON.Search, loading: false});
+  }
+
+  async handleSubmit(e){
+    e.preventDefault();
+    if(!this.state.searchTerm){
+      return this.setState({error: 'Place write a valid text'})
+    }
+
+    const res = await fetch(`${API}&s=${this.state.searchTerm}`)
+    const data = await res.json(); 
+
+    if(!data.Search){
+      return this.setState({error: 'there are no results'});
+    }
+
+
+    this.setState({data: data.Search, error: '', searchTerm: ''}); 
+  }
+
+  render() {
+
+    const {data, loading} = this.state;
+    if(loading){
+      return <h3 className="text-light">loading...</h3>
+    }
+
+
+    return(
+      <>
+            <div className="row">
+                <div className="col-md-4 offset-md-4 p-4">
+                  <form onSubmit={(e) => this.handleSubmit(e)}>
+                    <input 
+                    type="text" 
+                    className="form-control" 
+                    placeholder="search"
+                    onChange={e => this.setState({searchTerm: e.target.value})}
+                    value={this.state.searchTerm}
+                    autoFocus
+                     />
+                  </form>
+                  <p className="text-white">{this.state.error ? this.state.error : ''}</p>
+                </div>
+            </div>
+            <div className="row">
+              {           
+                    data.map((movie, i) => {
+                    return <Card movie={movie} key={i}/>;
+                  })         
+              }
+          </div>  
+      </>
+    )
+  }
+}
+
+export default List; 
